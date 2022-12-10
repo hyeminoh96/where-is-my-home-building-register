7729e5592b5c52b16599be9868327d8ed73a3ffa
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2022-12-10 15:12:04 +0900

    Update streamlit_app.py
    create mysql connection to streamlit

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,3 +1,27 @@
 import streamlit as st
+import mysql.connector
+
+
+# Initialize connection
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+
+conn = init_connection()
 
 st.header('Streamlit Test View')
+
+
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+
+rows = run_query("SELECT * FROM address_code limit 5;")
+
+# print results
+for row in rows:
+    st.write(row)
