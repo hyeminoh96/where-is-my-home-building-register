c78cc7db5a8b9eb140d526beff1f9d66c8b6bca9
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2022-12-20 23:07:36 +0900

    불필요한 ui 삭제

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -5,22 +5,20 @@ import pandas as pd
 from api_request import getBrTitleInfo
 from db import filter_open_column, get_sido_list, get_sigungu_list, get_bjdong_list, get_address_code
 
+st.header('🏡 건축물대장 조회')
 
 col1, col2, col3, col4 = st.columns(4)
 with col1:
     sido_list = get_sido_list()
     sido_option = st.selectbox('시도', tuple(sido_list))
-    st.write('선택된 시도:', sido_option)
 
 with col2:
     sigungu_list = get_sigungu_list(sido_option)
     sigungu_option = st.selectbox('시군구', tuple(sigungu_list))
-    st.write('선택된 시군구:', sigungu_option)
 
 with col3:
     bjdong_list = get_bjdong_list(sigungu_option)
     bjdong_option = st.selectbox('동읍면', tuple(bjdong_list))
-    st.write('선택된 동읍면:', bjdong_option)
 
 with col4:
     if st.button('조회'):
@@ -29,7 +27,7 @@ with col4:
         bld_df = filter_open_column(bld_df)
 
     else:
-        st.write('주소를 선택하세요.')
+        pass
 
 st.dataframe(data=bld_df)
 
