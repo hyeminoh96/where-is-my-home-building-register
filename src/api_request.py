12d22e7d4e9e19af82a090b96634eb9e3bbebdee
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2022-12-20 22:51:41 +0900

    api url https로 수정

--- src/api_request.py
+++ src/api_request.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def getBrTitleInfo(sigunguCd, bjdongCd, secret):
     service_key = secret['bldrgst_service_key']
-    url = 'http://apis.data.go.kr/1613000/BldRgstService_v2/getBrTitleInfo'
+    url = 'https://apis.data.go.kr/1613000/BldRgstService_v2/getBrTitleInfo'
     params = {'_type': 'json', 'serviceKey': service_key, 'sigunguCd': sigunguCd, 'bjdongCd': bjdongCd,
               'platGbCd': '0', 'bun': '', 'ji': '',
               'startDate': '', 'endDate': '', 'numOfRows': '10', 'pageNo': '1'}
