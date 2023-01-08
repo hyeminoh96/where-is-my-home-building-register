92290c37cdefaf3d1fdf2ed5a68bc7c041e5d902
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2023-01-08 14:33:20 +0900

    건축물대장 표제부 조회에서 전유부 조회로 변경
    - 표제부 : 집합건물 전체에 대한 내용이 기재되어 있음
    - 전유부 : 각 호수별 내용이 기재되어있음

--- src/api_request.py
+++ src/api_request.py
@@ -4,12 +4,17 @@ import pandas as pd
 
 def getBrTitleInfo(sigunguCd, bjdongCd, secret):
     service_key = secret['bldrgst_service_key']
-    url = 'https://apis.data.go.kr/1613000/BldRgstService_v2/getBrTitleInfo'
+    url = 'https://apis.data.go.kr/1613000/BldRgstService_v2/getBrExposInfo'
     params = {'_type': 'json', 'serviceKey': service_key, 'sigunguCd': sigunguCd, 'bjdongCd': bjdongCd,
-              'platGbCd': '0', 'bun': '', 'ji': '',
-              'startDate': '', 'endDate': '', 'numOfRows': '10', 'pageNo': '1'}
+              'platGbCd': '0',
+              # 'bun': '', 'ji': '',
+              'startDate': '', 'endDate': '', 'numOfRows': '2000', 'pageNo': '1'}
+    print("params")
+    print(params)
 
     response = requests.get(url, params=params)
+    print('response.json()')
+    print(response.json())
     response_json = response.json()['response']['body']['items']['item']
     response_df = pd.DataFrame(response_json)
     return response_df
