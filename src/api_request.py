c53a0d4c40afb6fad83f0932f45c8f85c3aec18c
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2023-01-08 15:40:18 +0900

    조건에 맞는 건축물대장 전체 호출
    1. 1개 행 호출해서 totalCount 가져옴
    2. totalCount // 1000을 페이지 번호로 파라미터에 넣고 for문으로 json에 append
    3. pandas df로 만들기

--- src/api_request.py
+++ src/api_request.py
@@ -7,14 +7,18 @@ def getBrTitleInfo(sigunguCd, bjdongCd, secret):
     url = 'https://apis.data.go.kr/1613000/BldRgstService_v2/getBrExposInfo'
     params = {'_type': 'json', 'serviceKey': service_key, 'sigunguCd': sigunguCd, 'bjdongCd': bjdongCd,
               'platGbCd': '0',
-              # 'bun': '', 'ji': '',
-              'startDate': '', 'endDate': '', 'numOfRows': '2000', 'pageNo': '1'}
+              'bun': '', 'ji': '',
+              'startDate': '', 'endDate': '', 'numOfRows': '1000', 'pageNo': '1'}
     print("params")
     print(params)
-
     response = requests.get(url, params=params)
-    print('response.json()')
-    print(response.json())
-    response_json = response.json()['response']['body']['items']['item']
-    response_df = pd.DataFrame(response_json)
-    return response_df
+    total_count = response.json()['response']['body']['totalCount']
+    print('total_count:', total_count)
+    result_json = []
+    for page_num in range(1, total_count // 1000 + 2):
+        params['pageNo'] = page_num
+        response = requests.get(url, params=params)
+        response_json = response.json()['response']['body']['items']['item']
+        result_json += response_json
+    result_df = pd.DataFrame(result_json)
+    return result_df
