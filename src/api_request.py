bd21cbec3226850c8656c302eb18e1228d831604
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2023-01-09 01:19:34 +0900

    건축소유자 정보 api 호출

--- src/api_request.py
+++ src/api_request.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import xmltodict
 
 
 def getBrTitleInfo(sigunguCd, bjdongCd, secret):
@@ -8,17 +9,35 @@ def getBrTitleInfo(sigunguCd, bjdongCd, secret):
     params = {'_type': 'json', 'serviceKey': service_key, 'sigunguCd': sigunguCd, 'bjdongCd': bjdongCd,
               'platGbCd': '0',
               'bun': '', 'ji': '',
-              'startDate': '', 'endDate': '', 'numOfRows': '1000', 'pageNo': '1'}
+              'startDate': '', 'endDate': '', 'numOfRows': '1000', 'pageNo': '1'}  # FIXME
     print("params")
     print(params)
     response = requests.get(url, params=params)
     total_count = response.json()['response']['body']['totalCount']
     print('total_count:', total_count)
     result_json = []
-    for page_num in range(1, total_count // 1000 + 2):
+    for page_num in range(1, total_count // 1000 + 2):  # FIXME
+        # for page_num in range(1, 2):
+        print('page_num:', page_num)
         params['pageNo'] = page_num
         response = requests.get(url, params=params)
         response_json = response.json()['response']['body']['items']['item']
         result_json += response_json
     result_df = pd.DataFrame(result_json)
+    return result_df
+
+
+def getArchitecturePossessionInfo(sigunguCd, bjdongCd, bld_df, secret):
+    service_key = secret['bldrgst_service_key']
+    url = 'https://apis.data.go.kr/1611000/OwnerInfoService/getArchitecturePossessionInfo'
+    params = {'ServiceKey': service_key, 'sigungu_cd': sigunguCd, 'bjdong_cd': bjdongCd,
+              'plat_gb_cd': '0', 'numOfRows': '1000', 'pageNo': '1', 'bun': ''}
+    result = []
+    for idx, bun in enumerate(bld_df['bun'].unique().tolist()):
+        print(f"bun_idx: {idx}/{len(bld_df['bun'].unique().tolist())}")
+        params['bun'] = bun
+        response = requests.get(url, params=params)
+        response_dict = xmltodict.parse(response.content)['response']['body']['items']['item']
+        result += response_dict
+    result_df = pd.DataFrame(result)
     return result_df
--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import io
 import pandas as pd
 
-from api_request import getBrTitleInfo
-from db import filter_open_column, get_sido_list, get_sigungu_list, get_bjdong_list, get_address_code
+from api_request import getBrTitleInfo, getArchitecturePossessionInfo
+from db import filter_open_column, get_sido_list, get_sigungu_list, get_bjdong_list, get_address_code, \
+    filter_owner_open_column
 
 st.header('🏡 건축물대장 조회')
 
@@ -24,16 +25,18 @@ with col4:
     if st.button('조회'):
         sigungu_code, bjdong_code = get_address_code(sido_option, sigungu_option, bjdong_option)
         bld_df = getBrTitleInfo(sigungu_code, bjdong_code, st.secrets['openapi'])
+        owner_df = getArchitecturePossessionInfo(sigungu_code, bjdong_code, bld_df, st.secrets['openapi'])
         bld_df = filter_open_column(bld_df)
-
+        owner_df = filter_owner_open_column(owner_df)
+        total_df = pd.merge(left=bld_df, right=owner_df, how='left', on='건축물대장번호')
     else:
         pass
 
-st.dataframe(data=bld_df)
+st.dataframe(data=total_df)
 
 buffer = io.BytesIO()
 with pd.ExcelWriter(buffer) as writer:
-    bld_df.to_excel(writer, index=False)
+    total_df.to_excel(writer, index=False)
     writer.save()
     st.download_button(label='📥엑셀로 다운로드', data=buffer,
                        file_name=f"건축물대장_{sido_option}_{sigungu_option}_{bjdong_option}.xlsx",
