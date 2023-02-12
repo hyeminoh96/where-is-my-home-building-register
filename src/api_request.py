4447669889ae10e92925d5e9aaba6647a2e6bffa
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2023-02-12 14:49:45 +0900

    request in asynchronous

--- src/api_request.py
+++ src/api_request.py
@@ -1,13 +1,13 @@
 import requests
-import pandas as pd
-import xmltodict
 import os
+import asyncio
 from time import time
 from dotenv import load_dotenv
 
 load_dotenv()
 service_key = os.environ.get('API_KEY')
 bld_rgst_url = 'https://apis.data.go.kr/1613000/BldRgstService_v2/getBrExposInfo'
+num_per_pg = 1000
 
 
 def get_total_count(sigungu_cd, bjdong_cd):
@@ -20,43 +20,34 @@ def get_total_count(sigungu_cd, bjdong_cd):
     return total_count
 
 
-def get_bld_rgst(sigungu_cd, bjdong_cd, total_count):
+async def get_bld_rgst(sigungu_cd, bjdong_cd, page_num):
     params = {'_type': 'json', 'serviceKey': service_key, 'sigunguCd': sigungu_cd, 'bjdongCd': bjdong_cd,
               'platGbCd': '0',
               'bun': '', 'ji': '',
-              'startDate': '', 'endDate': '', 'numOfRows': '1000', 'pageNo': '1'}
-    result_json = []
-    for page_num in range(1, total_count // 1000 + 2):
-        print('page_num:', page_num)
-        params['pageNo'] = page_num
-        response = requests.get(bld_rgst_url, params=params)
-        response_json = response.json()['response']['body']['items']['item']
-        result_json += response_json
-    # result_df = pd.DataFrame(result_json)
-    return result_json  # TODO: pandas 처리 분리
+              'startDate': '', 'endDate': '', 'numOfRows': num_per_pg, 'pageNo': page_num}
+
+    response = await loop.run_in_executor(None, requests.get, bld_rgst_url, params)
+    response_json = response.json()['response']['body']['items']['item']
+    return response_json  # TODO: pandas 처리 분리
 
 
-def getArchitecturePossessionInfo(sigunguCd, bjdongCd, bld_df, secret):
-    service_key = secret['bldrgst_service_key']
-    url = 'https://apis.data.go.kr/1611000/OwnerInfoService/getArchitecturePossessionInfo'
-    params = {'ServiceKey': service_key, 'sigungu_cd': sigunguCd, 'bjdong_cd': bjdongCd,
-              'plat_gb_cd': '0', 'numOfRows': '1000', 'pageNo': '1', 'bun': ''}
-    result = []
-    for idx, bun in enumerate(bld_df['bun'].unique().tolist()):
-        print(f"bun_idx: {idx}/{len(bld_df['bun'].unique().tolist())}")
-        params['bun'] = bun
-        response = requests.get(url, params=params)
-        response_dict = xmltodict.parse(response.content)['response']['body']['items']['item']
-        result += response_dict
-    result_df = pd.DataFrame(result)
-    return result_df
+async def gather_coroutines():
+    total_count = get_total_count(sigungu_cd, bjdong_cd)
+    futures = [asyncio.ensure_future(get_bld_rgst(sigungu_cd, bjdong_cd, pg_num)) for pg_num in
+               range(1, total_count // num_per_pg + 2)]
+
+    result = await asyncio.gather(*futures)
+    return result
 
 
 if __name__ == '__main__':
     start = time()
     sigungu_cd = 11230
     bjdong_cd = 10900
-    total_count = get_total_count(sigungu_cd, bjdong_cd)
-    get_bld_rgst(sigungu_cd, bjdong_cd, total_count)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(gather_coroutines())
+    loop.close()
+
     end = time()
     print(f'RUN TIME: {end - start}')
