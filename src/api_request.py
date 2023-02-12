cb6b66f44536afd5fc48403d6ad6980df52abcd1
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2023-02-12 13:54:14 +0900

    install dotenv and get api from .env

--- src/api_request.py
+++ src/api_request.py
@@ -1,30 +1,39 @@
 import requests
 import pandas as pd
 import xmltodict
+import os
+from time import time
+from dotenv import load_dotenv
+
+load_dotenv()
+service_key = os.environ.get('API_KEY')
+bld_rgst_url = 'https://apis.data.go.kr/1613000/BldRgstService_v2/getBrExposInfo'
 
 
-def getBrTitleInfo(sigunguCd, bjdongCd, secret):
-    service_key = secret['bldrgst_service_key']
-    url = 'https://apis.data.go.kr/1613000/BldRgstService_v2/getBrExposInfo'
-    params = {'_type': 'json', 'serviceKey': service_key, 'sigunguCd': sigunguCd, 'bjdongCd': bjdongCd,
+def get_total_count(sigungu_cd, bjdong_cd):
+    params = {'_type': 'json', 'serviceKey': service_key, 'sigunguCd': sigungu_cd, 'bjdongCd': bjdong_cd,
               'platGbCd': '0',
               'bun': '', 'ji': '',
-              'startDate': '', 'endDate': '', 'numOfRows': '1000', 'pageNo': '1'}  # FIXME
-    print("params")
-    print(params)
-    response = requests.get(url, params=params)
+              'startDate': '', 'endDate': '', 'numOfRows': '1', 'pageNo': '1'}
+    response = requests.get(bld_rgst_url, params=params)
     total_count = response.json()['response']['body']['totalCount']
-    print('total_count:', total_count)
+    return total_count
+
+
+def get_bld_rgst(sigungu_cd, bjdong_cd, total_count):
+    params = {'_type': 'json', 'serviceKey': service_key, 'sigunguCd': sigungu_cd, 'bjdongCd': bjdong_cd,
+              'platGbCd': '0',
+              'bun': '', 'ji': '',
+              'startDate': '', 'endDate': '', 'numOfRows': '1000', 'pageNo': '1'}
     result_json = []
-    for page_num in range(1, total_count // 1000 + 2):  # FIXME
-        # for page_num in range(1, 2):
+    for page_num in range(1, total_count // 1000 + 2):
         print('page_num:', page_num)
         params['pageNo'] = page_num
-        response = requests.get(url, params=params)
+        response = requests.get(bld_rgst_url, params=params)
         response_json = response.json()['response']['body']['items']['item']
         result_json += response_json
-    result_df = pd.DataFrame(result_json)
-    return result_df
+    # result_df = pd.DataFrame(result_json)
+    return result_json  # TODO: pandas 처리 분리
 
 
 def getArchitecturePossessionInfo(sigunguCd, bjdongCd, bld_df, secret):
@@ -41,3 +50,13 @@ def getArchitecturePossessionInfo(sigunguCd, bjdongCd, bld_df, secret):
         result += response_dict
     result_df = pd.DataFrame(result)
     return result_df
+
+
+if __name__ == '__main__':
+    start = time()
+    sigungu_cd = 11230
+    bjdong_cd = 10900
+    total_count = get_total_count(sigungu_cd, bjdong_cd)
+    get_bld_rgst(sigungu_cd, bjdong_cd, total_count)
+    end = time()
+    print(f'RUN TIME: {end - start}')
