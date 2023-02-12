589f68120329223ee29e207e7da1d55de213047e
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2023-02-12 15:23:01 +0900

    Move functions into a class

--- src/api_request.py
+++ src/api_request.py
@@ -4,50 +4,54 @@ import asyncio
 from time import time
 from dotenv import load_dotenv
 
-load_dotenv()
-service_key = os.environ.get('API_KEY')
-bld_rgst_url = 'https://apis.data.go.kr/1613000/BldRgstService_v2/getBrExposInfo'
-num_per_pg = 1000
 
+class GetBuildingRegister:
+    def __init__(self, sigungu_code, bjdong_code):
+        self.sigungu_cd = sigungu_code
+        self.bjdong_cd = bjdong_code
 
-def get_total_count(sigungu_cd, bjdong_cd):
-    params = {'_type': 'json', 'serviceKey': service_key, 'sigunguCd': sigungu_cd, 'bjdongCd': bjdong_cd,
-              'platGbCd': '0',
-              'bun': '', 'ji': '',
-              'startDate': '', 'endDate': '', 'numOfRows': '1', 'pageNo': '1'}
-    response = requests.get(bld_rgst_url, params=params)
-    total_count = response.json()['response']['body']['totalCount']
-    return total_count
+        load_dotenv()
+        self.service_key = os.environ.get('API_KEY')
+        self.bld_rgst_url = 'https://apis.data.go.kr/1613000/BldRgstService_v2/getBrExposInfo'
+        self.num_per_pg = 1000
 
+        self.loop = asyncio.get_event_loop()
 
-async def get_bld_rgst(sigungu_cd, bjdong_cd, page_num):
-    params = {'_type': 'json', 'serviceKey': service_key, 'sigunguCd': sigungu_cd, 'bjdongCd': bjdong_cd,
-              'platGbCd': '0',
-              'bun': '', 'ji': '',
-              'startDate': '', 'endDate': '', 'numOfRows': num_per_pg, 'pageNo': page_num}
+    def get_total_count(self):
+        params = {'_type': 'json', 'serviceKey': self.service_key, 'sigunguCd': self.sigungu_cd,
+                  'bjdongCd': self.bjdong_cd,
+                  'platGbCd': '0',
+                  'bun': '', 'ji': '',
+                  'startDate': '', 'endDate': '', 'numOfRows': '1', 'pageNo': '1'}
+        response = requests.get(self.bld_rgst_url, params=params)
+        total_count = response.json()['response']['body']['totalCount']
+        return total_count
 
-    response = await loop.run_in_executor(None, requests.get, bld_rgst_url, params)
-    response_json = response.json()['response']['body']['items']['item']
-    return response_json  # TODO: pandas 처리 분리
+    async def get_bld_rgst(self, page_num):
+        params = {'_type': 'json', 'serviceKey': self.service_key, 'sigunguCd': self.sigungu_cd,
+                  'bjdongCd': self.bjdong_cd,
+                  'platGbCd': '0',
+                  'bun': '', 'ji': '',
+                  'startDate': '', 'endDate': '', 'numOfRows': self.num_per_pg, 'pageNo': page_num}
 
+        response = await self.loop.run_in_executor(None, requests.get, self.bld_rgst_url, params)
+        response_json = response.json()['response']['body']['items']['item']
+        return response_json
 
-async def gather_coroutines():
-    total_count = get_total_count(sigungu_cd, bjdong_cd)
-    futures = [asyncio.ensure_future(get_bld_rgst(sigungu_cd, bjdong_cd, pg_num)) for pg_num in
-               range(1, total_count // num_per_pg + 2)]
+    async def gather_coroutines(self):
+        total_count = self.get_total_count()
+        futures = [asyncio.ensure_future(self.get_bld_rgst(pg_num)) for pg_num in
+                   range(1, total_count // self.num_per_pg + 2)]
+        result = await asyncio.gather(*futures)
+        return result
 
-    result = await asyncio.gather(*futures)
-    return result
+    def run(self):
+        self.loop.run_until_complete(self.gather_coroutines())
+        self.loop.close()
 
 
 if __name__ == '__main__':
     start = time()
-    sigungu_cd = 11230
-    bjdong_cd = 10900
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(gather_coroutines())
-    loop.close()
-
+    GetBuildingRegister(11230, 10900).run()
     end = time()
     print(f'RUN TIME: {end - start}')
