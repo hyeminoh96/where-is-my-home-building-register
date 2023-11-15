90c1d0f78e51c69dbdf50ad1a72cbb922400b638
Hyemin Oh <hyemin4246@gmail.com>
2023-11-15 22:46:29 +0900

    Get environment variables via from_object()

--- src/config.py
+++ src/config.py
@@ -1,14 +1,27 @@
 import os
 
-from dotenv import load_dotenv
 
-env_path = '.env'
-load_dotenv(dotenv_path=env_path)
+class Default:
+    API_KEY = os.environ.get("API_KEY")
+    MYSQL_HOST = os.environ.get("MYSQL_HOST")
+    MYSQL_PORT = os.environ.get("MYSQL_PORT")
+    MYSQL_USER = os.environ.get("MYSQL_USER")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+    MYSQL_SCHEMA_NAME = os.environ.get("MYSQL_SCHEMA_NAME")
 
-API_KEY = os.getenv("API_KEY")
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = os.getenv("MYSQL_PORT")
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_SCHEMA_NAME = os.getenv("MYSQL_SCHEMA_NAME")
+    SQLALCHEMY_DATABASE_URI = f'mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_SCHEMA_NAME}'
 
+
+app_config = {
+    'development': Default(),
+}
+
+config_name = os.environ.get('FLASK_CONFIG', 'development')
+
+
+def get_environment_variable(key: str):
+    env = app_config[config_name]
+    if env[key]:
+        return env[key]
+    else:
+        raise Exception(f"Not Set Environment Variable :: {key}")
--- src/presentation/flask/app.py
+++ src/presentation/flask/app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from src.config import MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_SCHEMA_NAME
+from src.config import app_config, config_name
 
-app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PASSWORD}/{MYSQL_SCHEMA_NAME}"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-db.create_all()
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+
+    db = SQLAlchemy(app=app)
+    db.create_all()
+    return app
+
+
+app = create_app(config_name)
 
 
 @app.route('/')
@@ -16,10 +21,10 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/address/sido')
-def get_sido():
-    sido_list = address_service.get_sido()
-    return
+# @app.route('/address/sido')
+# def get_sido():
+#     sido_list = address_service.get_sido()
+#     return
 
 
 if __name__ == '__main__':
