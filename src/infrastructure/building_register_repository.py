e7ee0c3592a7819faa5a9184f34290e292b25b4b
Hyemin Oh <hyemin4246@gmail.com>
2023-11-29 06:46:38 +0900

    건축물대장 repository 구조 정리

---
+++ src/infrastructure/building_register_repository.py
@@ -0,0 +1,35 @@
+import os
+
+from src.infrastructure.utils import get_request
+
+
+class BuildingRegisterRepository:
+    url = 'https://apis.data.go.kr/1613000/BldRgstService_v2/getBrExposInfo'
+    service_key = os.environ.get('API_KEY')
+
+    def __init__(self, sigungu_code, bjdong_code):
+        self.sigungu_cd = sigungu_code
+        self.bjdong_cd = bjdong_code
+        self.num_per_pg = 1000
+
+    def get_total_count(self):
+        params = {'_type': 'json', 'serviceKey': self.service_key, 'sigunguCd': self.sigungu_cd,
+                  'bjdongCd': self.bjdong_cd,
+                  'platGbCd': '0',
+                  'bun': '', 'ji': '',
+                  'startDate': '', 'endDate': '', 'numOfRows': '1', 'pageNo': '1'}
+        response = get_request(self.url, params=params)
+        response_json = response.json()
+        total_count = response_json['response']['body']['totalCount']
+        return total_count
+
+    def get_bld_rgst(self, page_num):
+        params = {'_type': 'json', 'serviceKey': self.service_key, 'sigunguCd': self.sigungu_cd,
+                  'bjdongCd': self.bjdong_cd,
+                  'platGbCd': '0',
+                  'bun': '', 'ji': '',
+                  'startDate': '', 'endDate': '', 'numOfRows': self.num_per_pg, 'pageNo': page_num}
+
+        response = get_request(self.url, params)
+        response_json = response.json()['response']['body']['items']['item']
+        return response_json
