06fee614209866560cbbcba88d11c14f8b739970
Hyemin Oh <hyemin4246@gmail.com>
2023-12-19 06:38:16 +0900

    파라미터로 받은 필드의 데이터만 reponse

--- src/application/building_register_service.py
+++ src/application/building_register_service.py
@@ -1,28 +1,41 @@
 import asyncio
 import itertools
+from dataclasses import dataclass
+from typing import List
+
 import pandas as pd
 
 from src.infrastructure.building_register_repository import BuildingRegisterRepository
-from src.infrastructure.register_columns_repository import RegisterColumnsRepository
+from src.infrastructure.register_columns_repository import RegisterColumnsRepository, RegisterTitleColumns
+
+
+@dataclass
+class RegisterTitleColumnsResponse:
+    columns: List[RegisterTitleColumns]
 
 
 class BuildingRegisterService:
 
     @staticmethod
-    def get_title_registers(sigungu_code, bjdong_code):
+    def get_title_registers(sigungu_code, bjdong_code, columns):
         register_repo = BuildingRegisterRepository()
         registers = asyncio.run(register_repo.async_request_total_registers(sigungu_code, bjdong_code))
 
-        # process with pandas
         flattened_results = list(itertools.chain.from_iterable(registers))
         df = pd.DataFrame.from_records(flattened_results)
-        return flattened_results
+
+        columns_code = [column['eng'] for column in columns]
+        df = df[columns_code]
+
+        # Rename columns in Korean
+        mapped_columns = {columns['eng']: columns['kor'] for columns in columns}
+
+        df.rename(columns=mapped_columns, inplace=True)
+
+        return df.to_json(orient='records', force_ascii=False)
 
     def get_columns(self):
         columns_repo = RegisterColumnsRepository()
         columns = columns_repo.query_columns()
-        return columns
 
-
-
-
+        return RegisterTitleColumnsResponse(columns=columns)
