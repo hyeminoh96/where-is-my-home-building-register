f2c61e7ba0d6083d43281e9fd1b320b0a2e88b59
Hyemin Oh <hyemin4246@gmail.com>
2023-12-19 06:38:27 +0900

    dataclass 적용 시도

--- src/infrastructure/register_columns_repository.py
+++ src/infrastructure/register_columns_repository.py
@@ -1,11 +1,21 @@
+from dataclasses import dataclass
+
+from typing import List
+
 from src.domain.building_register_columns_info import RegisterTitleColumnsMapper
 from src.infrastructure.db import db
 
 
+@dataclass
+class RegisterTitleColumns:
+    eng: str
+    kor: str
+
+
 class RegisterColumnsRepository:
-    def query_columns(self):
+    def query_columns(self) -> List[RegisterTitleColumns]:
         result = db.session.execute(
             db.select(RegisterTitleColumnsMapper.eng, RegisterTitleColumnsMapper.kor).filter_by(
                 status='open')).all()
-
+        result = [RegisterTitleColumns(eng=row[0], kor=row[1]) for row in result]
         return result
