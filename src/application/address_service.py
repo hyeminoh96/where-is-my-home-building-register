6b396b44bfce8fe488a773cbf68db244cdc7001e
Hyemin Oh <hyemin4246@gmail.com>
2023-12-30 17:17:59 +0900

    법정동 목록 조회 response 구조 변경

--- src/application/address_service.py
+++ src/application/address_service.py
@@ -4,18 +4,18 @@ from src.infrastructure.address_repository import AddressRepository
 class AddressService:
     address_repo = AddressRepository()
 
-    def get_sido(self):
+    def get_sido(self) -> dict:
         sido_list = self.address_repo.query_sido_list()
         return sido_list
 
-    def get_sigungu(self, sido):
+    def get_sigungu(self, sido) -> dict:
         sigungu_list = self.address_repo.query_sigungu_list(sido)
         return sigungu_list
 
-    def get_bjdong(self, sido, sigungu):
+    def get_bjdong(self, sido, sigungu) -> dict:
         bjdong_list = self.address_repo.query_bjdong_list(sido, sigungu)
         return bjdong_list
 
-    def get_code(self, sido, sigungu, bjdong) -> tuple:
-        sigungu_code, bjdong_code = self.address_repo.query_code(sido, sigungu, bjdong)
-        return sigungu_code, bjdong_code
+    def get_code(self, sido, sigungu, bjdong) -> dict:
+        code_result = self.address_repo.query_code(sido, sigungu, bjdong)
+        return code_result
